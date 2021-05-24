440f64e636384a534cdc57e3a65a20f1ad6bec0b
Philip Okiokio <philipokiokio@gmail.com>
2021-05-24 10:40:37 +0100

    test

--- keyvalue/tests.py
+++ keyvalue/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.http import response
+from django.test import TestCase,Client
+from .models import TimeUUID
+from django.urls import reverse
+
+from .models import TimeUUID
+
 
 # Create your tests here.
+class ApiViewTest(TestCase):
+
+    def setUp(cls):
+        text = 'we'
+
+
+    def test_list_view(self):
+        response = self.client.get(reverse('key_value'))
+        self.assertEqual(response.status_code, 200)
+        
+
+    def test_create_view(self):
+        response = self.client.post(reverse('create'),
+        {'text': 'ew'
+        })
+        self.assertRedirects(response,'/api/v1/',status_code=302,target_status_code=200, fetch_redirect_response=True)
