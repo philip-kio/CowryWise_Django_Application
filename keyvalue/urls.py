51a7f73d31a832498bfde65f32d900e300eb14ad
Philip Okiokio <philipokiokio@gmail.com>
2021-05-29 13:42:35 +0100

    Function based

--- keyvalue/urls.py
+++ keyvalue/urls.py
@@ -1,9 +1,9 @@
 from django.db.models.base import Model
 from django.urls import path
-from .views import ListTimeUuid, createTimeView
+from .views import  createTimeView
 
 urlpatterns = [
-    path('', ListTimeUuid.as_view(), name= 'key_value'), 
+
     path('list/', createTimeView, name='create')
 
 ]
--- keyvalue/views.py
+++ keyvalue/views.py
@@ -2,26 +2,40 @@ from rest_framework import generics
 from .models import TimeUUID
 from .serializers import TimeUUIDPostSerializer,TimeUUIDGetSerializer
 from rest_framework.response import Response
-from django.shortcuts import redirect
+
 from rest_framework.decorators import api_view
+
 # Create your views here.
 
-class ListTimeUuid(generics.ListAPIView):
-    queryset = TimeUUID.objects.all().order_by('-time_date')
-    serializer_class = TimeUUIDGetSerializer
 
 
 
+@api_view(['GET','POST'])
 
-@api_view(['POST'])
 def createTimeView(request):
     if request.method == "POST":
+        
         serializer = TimeUUIDPostSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
         serializer.save()
 
         if serializer.save():
-            return redirect('key_value')
+            queryset = TimeUUID.objects.order_by('-time_date')
+            serializer = TimeUUIDGetSerializer(queryset, many=True)
+            return Response(serializer.data)
+    
+    elif request.method == "GET":
+        request.method = "POST"
+        serializer = TimeUUIDPostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+
+        if serializer.save():
+            queryset = TimeUUID.objects.order_by('-time_date')
+            serializer = TimeUUIDGetSerializer(queryset, many=True)
+            return Response(serializer.data)
+    
     else:
         return Response({'Response': 'did not work'})
